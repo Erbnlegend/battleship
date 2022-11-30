df30f5ae2c61b4e277dddd7751567206321cf286
Aaron Erb <erbnlegend@gmail.com>
2022-11-29 18:04:24 -0600

    Testing computer issues with player grid

--- src/components/players.js
+++ src/components/players.js
@@ -1,9 +1,9 @@
-import { Ships } from './ships'
+import { ShipsFactory } from './ships'
 import { GameBoard } from './gameBoard'
 
 const player1 = {
   name: 'player1',
-  Ships,
+  Ships: ShipsFactory(),
   attack: function (coords) {
     console.log(`${coords} attacked`)
   },
@@ -16,84 +16,83 @@ const player1 = {
 }
 const player2 = {
   name: 'player2',
-  Ships,
+  Ships: ShipsFactory(),
   attack: function (coords) {
     console.log(`${coords} attacked`)
   },
-  deploy: function () {
-    const coordsArray = []
-    for (const item in player2.Ships) {
-      const shipName = player2.Ships[item].name[0].toLowerCase() + player2.Ships[item].name.slice(1)
+  coordsArray: [],
+  deploy () {
+    for (const item in this.Ships) {
+      const shipName = this.Ships[item].name[0].toLowerCase() + this.Ships[item].name.slice(1)
       // Store on ship for placement
       let randomX = Math.floor(Math.random() * 10)
       let randomY = Math.floor(Math.random() * 10)
       const check = `x: ${randomX}, y: ${randomY}`
-      if (coordsArray.includes(check)) {
+      if (this.coordsArray.includes(check)) {
         randomX = Math.floor(Math.random() * 10)
         randomY = Math.floor(Math.random() * 10)
       }
-      coordsArray.push(check)
-      player2.Ships[item].x = randomX
-      player2.Ships[item].y = randomY
+      this.coordsArray.push(check)
+      this.Ships[item].x = randomX
+      this.Ships[item].y = randomY
 
-      checkDeployment(shipName)
+      this.checkDeployment(shipName)
+
+      console.log(this.Ships[item].name, this.Ships[item].x, this.Ships[item].y)
     }
-    function checkDeployment (shipName) {
-      console.log(`${player2.Ships[shipName].name}`, player2.Ships[shipName].moves)
-      if (!player2.Ships[shipName].moves.forwards.allowed && !player2.Ships[shipName].moves.backwards.allowed && !player2.Ships[shipName].moves.up.allowed && !player2.Ships[shipName].moves.down.allowed) {
-        player2.reset(shipName)
-        console.log('reset')
+  },
+  checkDeployment (shipName) {
+    console.log(`${this.Ships[shipName].name}`, this.Ships[shipName].moves)
+    // Make sure that all move options are not false
+    if (!this.Ships[shipName].moves.forwards.allowed && !this.Ships[shipName].moves.backwards.allowed && !this.Ships[shipName].moves.up.allowed && !this.Ships[shipName].moves.down.allowed) {
+      this.reset(shipName)
+      console.log('reset')
 
-        let randomX = Math.floor(Math.random() * 10)
-        let randomY = Math.floor(Math.random() * 10)
-        const check = `x: ${randomX}, y: ${randomY}`
-        if (coordsArray.includes(check)) {
-          randomX = Math.floor(Math.random() * 10)
-          randomY = Math.floor(Math.random() * 10)
-        }
-        coordsArray.push(check)
-        player2.Ships[shipName].x = randomX
-        player2.Ships[shipName].y = randomY
-      } else if (player2.Ships[shipName].moves.up.allowed) {
-        const direction = player2.Ships[shipName].moves.up
-        const coords = { x: player2.Ships[shipName].x, y: player2.Ships[shipName].y }
-        console.log(coords)
-        const element = null
-
-        GameBoard.determineShipOrientation(shipName, coords, element, player2)
-        GameBoard.deployShip(direction, shipName, player2, coords)
-
-        // check if backwards is allowed
-      } else if (player2.Ships[shipName].moves.backwards.allowed) {
-        const direction = player2.Ships[shipName].moves.backwards
-        const coords = { x: player2.Ships[shipName].x, y: player2.Ships[shipName].y }
-        console.log(coords)
-        const element = null
-
-        GameBoard.determineShipOrientation(shipName, coords, element, player2)
-        GameBoard.deployShip(direction, shipName, player2, coords)
-
-        // Checks if down is allowed
-      } else if (player2.Ships[shipName].moves.down.allowed) {
-        const direction = player2.Ships[shipName].moves.down
-        const coords = { x: player2.Ships[shipName].x, y: player2.Ships[shipName].y }
-        console.log(coords)
-        const element = null
-
-        GameBoard.determineShipOrientation(shipName, coords, element, player2)
-        GameBoard.deployShip(direction, shipName, player2, coords)
-        // Checks is forwards is allowed
-      } else if (player2.Ships[shipName].moves.forwards.allowed) {
-        const direction = player2.Ships[shipName].moves.forwards
-        const coords = { x: player2.Ships[shipName].x, y: player2.Ships[shipName].y }
-        console.log(coords)
-        const element = null
-
-        GameBoard.determineShipOrientation(shipName, coords, element, player2)
-        GameBoard.deployShip(direction, shipName, player2, coords)
+      let randomX = Math.floor(Math.random() * 10)
+      let randomY = Math.floor(Math.random() * 10)
+      const check = `x: ${randomX}, y: ${randomY}`
+      if (this.coordsArray.includes(check)) {
+        randomX = Math.floor(Math.random() * 10)
+        randomY = Math.floor(Math.random() * 10)
       }
+      this.coordsArray.push(check)
+      this.Ships[shipName].x = randomX
+      this.Ships[shipName].y = randomY
+    } else if (this.Ships[shipName].moves.up.allowed) {
+      const direction = this.Ships[shipName].moves.up
+      const coords = { x: this.Ships[shipName].x, y: this.Ships[shipName].y }
+      const element = null
+
+      GameBoard.determineShipOrientation(shipName, coords, element, this)
+      GameBoard.deployShip(direction, shipName, this, coords)
+
+      // check if backwards is allowed
+    } else if (this.Ships[shipName].moves.backwards.allowed) {
+      const direction = this.Ships[shipName].moves.backwards
+      const coords = { x: this.Ships[shipName].x, y: this.Ships[shipName].y }
+      const element = null
+
+      GameBoard.determineShipOrientation(shipName, coords, element, this)
+      GameBoard.deployShip(direction, shipName, this, coords)
+
+      // Checks if down is allowed
+    } else if (this.Ships[shipName].moves.down.allowed) {
+      const direction = this.Ships[shipName].moves.down
+      const coords = { x: this.Ships[shipName].x, y: this.Ships[shipName].y }
+      const element = null
+
+      GameBoard.determineShipOrientation(shipName, coords, element, this)
+      GameBoard.deployShip(direction, shipName, this, coords)
+      // Checks is forwards is allowed
+    } else if (this.Ships[shipName].moves.forwards.allowed) {
+      const direction = this.Ships[shipName].moves.forwards
+      const coords = { x: this.Ships[shipName].x, y: this.Ships[shipName].y }
+      const element = null
+
+      GameBoard.determineShipOrientation(shipName, coords, element, this)
+      GameBoard.deployShip(direction, shipName, this, coords)
     }
-    console.log(`${this.name}`, GameBoard[`${this.name}Grid`])
+    console.log(GameBoard.player2Grid)
   },
   reset: function (ship) {
     this.Ships[ship].moves.forwards.allowed = true
--- src/components/ships.js
+++ src/components/ships.js
@@ -1,145 +1,146 @@
-const Ships = {
-  carrier: {
-    name: 'Carrier',
-    url: './imgs/aircraftcarrierBig.svg',
-    length: 5,
-    hitPoints: 5,
-    sunk: false,
-    condition: '',
-    moves: {
-      forwards: { text: 'forwards', allowed: true, deg: 0, origin: 'center left' },
-      backwards: { text: 'backwards', allowed: true, deg: -180, origin: '30px' },
-      up: { text: 'up', allowed: true, deg: -90, origin: '30px' },
-      down: { text: 'down', allowed: true, deg: 90, origin: '30px' }
-    },
-    x: '',
-    y: '',
-    hit: function () {
-      if (this.hitPoints === 0) {
-        return
+function ShipsFactory () {
+  return {
+    carrier: {
+      name: 'Carrier',
+      url: './imgs/aircraftcarrierBig.svg',
+      length: 5,
+      hitPoints: 5,
+      sunk: false,
+      condition: '',
+      moves: {
+        forwards: { text: 'forwards', allowed: true, deg: 0, origin: 'center left' },
+        backwards: { text: 'backwards', allowed: true, deg: -180, origin: '30px' },
+        up: { text: 'up', allowed: true, deg: -90, origin: '30px' },
+        down: { text: 'down', allowed: true, deg: 90, origin: '30px' }
+      },
+      x: '',
+      y: '',
+      hit: function () {
+        if (this.hitPoints === 0) {
+          return
+        }
+        if (this.hitPoints === 1) {
+          this.sunkShip()
+        }
+        this.hitPoints--
+      },
+      sunkShip: function () {
+        this.sunk = true
       }
-      if (this.hitPoints === 1) {
-        this.sunkShip()
-      }
-      this.hitPoints--
     },
-    sunkShip: function () {
-      this.sunk = true
-    }
-  },
-  battleship: {
-    name: 'Battleship',
-    url: './imgs/battleshipBig.svg',
-    length: 4,
-    hitPoints: 4,
-    sunk: false,
-    condition: '',
-    moves: {
-      forwards: { text: 'forwards', allowed: true, deg: 0, origin: 'center left' },
-      backwards: { text: 'backwards', allowed: true, deg: -180, origin: '30px' },
-      up: { text: 'up', allowed: true, deg: -90, origin: '30px' },
-      down: { text: 'down', allowed: true, deg: 90, origin: '30px' }
-    },
-    x: '',
-    y: '',
-    hit: function () {
-      if (this.hitPoints === 0) {
-        return
+    battleship: {
+      name: 'Battleship',
+      url: './imgs/battleshipBig.svg',
+      length: 4,
+      hitPoints: 4,
+      sunk: false,
+      condition: '',
+      moves: {
+        forwards: { text: 'forwards', allowed: true, deg: 0, origin: 'center left' },
+        backwards: { text: 'backwards', allowed: true, deg: -180, origin: '30px' },
+        up: { text: 'up', allowed: true, deg: -90, origin: '30px' },
+        down: { text: 'down', allowed: true, deg: 90, origin: '30px' }
+      },
+      x: '',
+      y: '',
+      hit: function () {
+        if (this.hitPoints === 0) {
+          return
+        }
+        if (this.hitPoints === 1) {
+          this.sunkShip()
+        }
+        this.hitPoints--
+      },
+      sunkShip: function () {
+        this.sunk = true
       }
-      if (this.hitPoints === 1) {
-        this.sunkShip()
-      }
-      this.hitPoints--
     },
-    sunkShip: function () {
-      this.sunk = true
-    }
-  },
-  destroyer: {
-    name: 'Destroyer',
-    url: './imgs/destroyerBig.svg',
-    length: 3,
-    hitPoints: 3,
-    sunk: false,
-    condition: '',
-    moves: {
-      forwards: { text: 'forwards', allowed: true, deg: 0, origin: 'center left' },
-      backwards: { text: 'backwards', allowed: true, deg: -180, origin: '30px' },
-      up: { text: 'up', allowed: true, deg: -90, origin: '30px' },
-      down: { text: 'down', allowed: true, deg: 90, origin: '30px' }
-    },
-    x: '',
-    y: '',
-    hit: function () {
-      if (this.hitPoints === 0) {
-        return
+    destroyer: {
+      name: 'Destroyer',
+      url: './imgs/destroyerBig.svg',
+      length: 3,
+      hitPoints: 3,
+      sunk: false,
+      condition: '',
+      moves: {
+        forwards: { text: 'forwards', allowed: true, deg: 0, origin: 'center left' },
+        backwards: { text: 'backwards', allowed: true, deg: -180, origin: '30px' },
+        up: { text: 'up', allowed: true, deg: -90, origin: '30px' },
+        down: { text: 'down', allowed: true, deg: 90, origin: '30px' }
+      },
+      x: '',
+      y: '',
+      hit: function () {
+        if (this.hitPoints === 0) {
+          return
+        }
+        if (this.hitPoints === 1) {
+          this.sunkShip()
+        }
+        this.hitPoints--
+      },
+      sunkShip: function () {
+        this.sunk = true
       }
-      if (this.hitPoints === 1) {
-        this.sunkShip()
-      }
-      this.hitPoints--
     },
-    sunkShip: function () {
-      this.sunk = true
-    }
-  },
-  warship: {
-    name: 'Warship',
-    url: '../imgs/warshipBig.svg',
-    length: 3,
-    hitPoints: 3,
-    sunk: false,
-    condition: '',
-    moves: {
-      forwards: { text: 'forwards', allowed: true, deg: 0, origin: 'center left' },
-      backwards: { text: 'backwards', allowed: true, deg: -180, origin: '30px' },
-      up: { text: 'up', allowed: true, deg: -90, origin: '30px' },
-      down: { text: 'down', allowed: true, deg: 90, origin: '30px' }
-    },
-    x: '',
-    y: '',
-    hit: function () {
-      if (this.hitPoints === 0) {
-        return
+    warship: {
+      name: 'Warship',
+      url: '../imgs/warshipBig.svg',
+      length: 3,
+      hitPoints: 3,
+      sunk: false,
+      condition: '',
+      moves: {
+        forwards: { text: 'forwards', allowed: true, deg: 0, origin: 'center left' },
+        backwards: { text: 'backwards', allowed: true, deg: -180, origin: '30px' },
+        up: { text: 'up', allowed: true, deg: -90, origin: '30px' },
+        down: { text: 'down', allowed: true, deg: 90, origin: '30px' }
+      },
+      x: '',
+      y: '',
+      hit: function () {
+        if (this.hitPoints === 0) {
+          return
+        }
+        if (this.hitPoints === 1) {
+          this.sunkShip()
+        }
+        this.hitPoints--
+      },
+      sunkShip: function () {
+        this.sunk = true
       }
-      if (this.hitPoints === 1) {
-        this.sunkShip()
-      }
-      this.hitPoints--
     },
-    sunkShip: function () {
-      this.sunk = true
-    }
-  },
-  patrol: {
-    name: 'Patrol',
-    url: './imgs/patrolBig.svg',
-    length: 2,
-    hitPoints: 2,
-    sunk: false,
-    condition: '',
-    moves: {
-      forwards: { text: 'forwards', allowed: true, deg: 0, origin: 'center left' },
-      backwards: { text: 'backwards', allowed: true, deg: -180, origin: '30px' },
-      up: { text: 'up', allowed: true, deg: -90, origin: '30px' },
-      down: { text: 'down', allowed: true, deg: 90, origin: '30px' }
-    },
-    x: '',
-    y: '',
-    hit: function () {
-      if (this.hitPoints === 0) {
-        return
+    patrol: {
+      name: 'Patrol',
+      url: './imgs/patrolBig.svg',
+      length: 2,
+      hitPoints: 2,
+      sunk: false,
+      condition: '',
+      moves: {
+        forwards: { text: 'forwards', allowed: true, deg: 0, origin: 'center left' },
+        backwards: { text: 'backwards', allowed: true, deg: -180, origin: '30px' },
+        up: { text: 'up', allowed: true, deg: -90, origin: '30px' },
+        down: { text: 'down', allowed: true, deg: 90, origin: '30px' }
+      },
+      x: '',
+      y: '',
+      hit: function () {
+        if (this.hitPoints === 0) {
+          return
+        }
+        if (this.hitPoints === 1) {
+          this.sunkShip()
+        }
+        this.hitPoints--
+      },
+      sunkShip: function () {
+        this.sunk = true
       }
-      if (this.hitPoints === 1) {
-        this.sunkShip()
-      }
-      this.hitPoints--
-    },
-    sunkShip: function () {
-      this.sunk = true
     }
   }
-
 }
 
-export { Ships }
+export { ShipsFactory }
